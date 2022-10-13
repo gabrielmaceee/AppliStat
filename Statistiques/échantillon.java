b4ad03b1b1d85ecda8c11b0144f6adfd61beeb55
cedricVel <112876757+cedricVel@users.noreply.github.com>
2022-10-13 10:31:35 +0200

    Update échantillon.java

--- Statistiques/échantillon.java
+++ Statistiques/échantillon.java
@@ -3,63 +3,64 @@ package Statistiques;
 import java.util.Arrays;
 
 class echantillon {
-double[] donnees;
-int taille;
-double[] donneesTriees;
+	
+	double[] donnees;
+	int taille;
+	double[] donneesTriees;
 
-echantillon(double [] TabDonnees) {
-	if(TabDonnees.length ==0) {
-	this.donnees = new double[0];
-	taille = 0;
-	throw new IllegalArgumentException("le tableau est vide");
+	echantillon(double [] TabDonnees) {
+		if(TabDonnees.length ==0) {
+			this.donnees = new double[0];
+			taille = 0;
+			throw new IllegalArgumentException("le tableau est vide");
+		} else { 
+			taille = TabDonnees.length;
+			this.donnees = TabDonnees;
+		}	
+		donneesTriees = donnees;
+		Arrays.sort(donneesTriees);
 	}
 
-	else { taille = TabDonnees.length;
-	this.donnees = TabDonnees;
-	}
-	donneesTriees = donnees;
-	Arrays.sort(donneesTriees);
-}
 
-
-double getMoyenne(){
-	double moyenne = 0;
-	for(int i = 0; i<taille; i ++) {
+	double getMoyenne(){
+		double moyenne = 0;
+		for(int i = 0; i<taille; i ++) {
 		moyenne += donnees[i];
+		}
+		return moyenne/taille;
 	}
-	return moyenne/taille;
-}
-double getVariance(){
-	double var = 0;
-	for(int i = 0; i<taille; i ++) {
-		var += Math.pow((donnees[i]- getMoyenne()),2);
+	double getVariance(){
+		double var = 0;
+		for(int i = 0; i<taille; i ++) {
+			var += Math.pow((donnees[i]- getMoyenne()),2);
+		}
+		return var/taille;
 	}
-	return var/taille;
-}
-double getEcartype(){
-	return Math.sqrt(getVariance());
-}
-double getMaximum(){
-	return donneesTriees[taille-1];
-};
-double getMinimum() {
-	return donneesTriees[0];
-};
-double getMediane() {
-	if (taille % 2 ==0) return donneesTriees[taille/2];
-	int m = taille/2;
-	return (donneesTriees[m]+donneesTriees[m+1])/2;
-}
-double getSCT() {
-	double SCT = 0;
-	for (int i =0; i<taille; i++) {
-		SCT += Math.pow((donnees[i] -getMoyenne()),2);
+	double getEcartype(){
+		return Math.sqrt(getVariance());
 	}
-	return SCT;
-}
+	double getMaximum(){
+		return donneesTriees[taille-1];
+	};
+	
+	double getMinimum() {
+		return donneesTriees[0];
+	};
+	double getMediane() {
+		if (taille % 2 ==0) return donneesTriees[taille/2];
+		int m = taille/2;
+		return (donneesTriees[m]+donneesTriees[m+1])/2;
+	}
+	double getSCT() {
+		double SCT = 0;
+		for (int i =0; i<taille; i++) {
+			SCT += Math.pow((donnees[i] -getMoyenne()),2);
+		}
+		return SCT;
+	}
 
-double getQuantiles(int quantile) {};
-
+	double getQuantiles(int quantile) {};
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
