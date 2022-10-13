834ca34ad49abb18f6a86793340235eca79941a5
Gabriel Macé <macegabriel7238@gmail.com>
2022-10-13 19:15:37 +0200

    Ajout constructeur Echantillon(String s)

---
+++ Statistiques/Echantillon.java
@@ -0,0 +1,115 @@
+package Statistiques;
+
+import java.util.Arrays;
+
+class Echantillon {
+	
+double[] donnees;
+int taille;
+double[] donneesTriees;
+
+
+    Echantillon(String s){
+    	donnees = new double[0];
+    	String d = new String();
+    	for (int i=0; i<s.length(); i++) {
+    		if(s.charAt(i)== ';')  {
+    			donnees = Arrays.copyOf(donnees, taille+1);
+        		donnees[taille]= Double.parseDouble(d);
+        		taille += 1;
+        		d = "";
+        		i++;
+    		}
+    		else if (i == s.length()-1){
+    			d += s.substring(i, i+1);
+    			donnees = Arrays.copyOf(donnees, taille+1);
+        		donnees[taille]= Double.parseDouble(d);
+        		taille += 1;
+    		}
+    		else d += s.substring(i,i+1);
+    	}
+    	donneesTriees = Arrays.copyOf(donnees, taille);
+    }
+    
+    
+	Echantillon(double [] TabDonnees) {
+		if(TabDonnees.length ==0) {
+			throw new IllegalArgumentException("le tableau est vide");
+		} else { 
+			taille = TabDonnees.length;
+			this.donnees = TabDonnees;
+		}	
+		donneesTriees = Arrays.copyOf(donnees,taille);
+		Arrays.sort(donneesTriees);
+	}
+
+
+	double getMoyenne(){
+		double moyenne = 0;
+		for(int i = 0; i<taille; i ++) {
+		moyenne += donnees[i];
+		}
+		return moyenne/taille;
+	}
+	double getVariance(){
+		double var = 0;
+		for(int i = 0; i<taille; i ++) {
+			var += Math.pow((donnees[i]- getMoyenne()),2);
+		}
+		return var/(taille-1);
+	}
+	double getEcarttype(){
+		return Math.sqrt(getVariance());
+	}
+	double getMaximum(){
+		return donneesTriees[taille-1];
+	};
+	
+	double getMinimum() {
+		return donneesTriees[0];
+	};
+	double getMediane() {
+		if (taille % 2 ==0) return donneesTriees[taille/2];
+		int m = taille/2;
+		return (donneesTriees[m]+donneesTriees[m+1])/2;
+	}
+	double getSCT() {
+		double SCT = 0;
+		for (int i =0; i<taille; i++) {
+			SCT += Math.pow((donnees[i] -getMoyenne()),2);
+		}
+		return SCT;
+	}
+	double getQuartiles(int quantile) {
+		if(quantile >= 4 ) throw new IllegalArgumentException();
+		if(quantile == 2) return this.getMediane();
+		float q = (float) ((quantile)*(taille+1)/4.0)-1;
+		if(q-((int)q)==0)return donneesTriees[(int)q];
+		int qa = (int)q+1;
+		return donneesTriees[qa];
+	}
+	public String toString() {
+		String s ="";
+		for(int i=0; i<taille; i++) {
+			if(i!= taille-1) s += donnees[i] + ", ";
+			else s += donnees[i];
+		}
+		return s;
+	}
+
+	//double getQuantiles(int quantile) {};
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double [] i1 = {1.52, 1.56, 1.54};
+		Echantillon v1 = new Echantillon(i1);
+		String s = new String("1.2; 3.4567");
+		Echantillon v2 = new Echantillon(s);
+		//System.out.println(v1.toString());
+		System.out.println(v2.toString());
+		System.out.println(v2.getSCT());
+		System.out.println(v2.getQuartiles(3));
+
+	}
+
+}
