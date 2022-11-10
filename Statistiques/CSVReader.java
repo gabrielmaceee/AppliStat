8b9ad3fff183d86b83030a000f68d77193330a66
Vassili <vazarch@protonmail.com>
2022-11-10 17:17:38 +0100

    Création accès test populaire + test mémoire courte

--- Statistiques/CSVReader.java
+++ Statistiques/CSVReader.java
@@ -1,29 +1,60 @@
 package Statistiques;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CSVReader {
+    static String csvFile= "C:/Users/8ni8n/OneDrive/Bureau/language de programmation/AppliStat/data/TableauFisher(1).csv";;
+    static String csvSave = "C:/Users/8ni8n/OneDrive/Bureau/language de programmation/AppliStat/data/Save.txt";
+    static List<String[]> data=new ArrayList<>();
+    static String cvsSplitBy = ",";
 
     public static double getQuantile(int a1, int na) {
-
-        String csvFile = "C:/Users/8ni8n/OneDrive/Bureau/language de programmation/AppliStat/data/TableauFisher(1).csv";
+        File csvSafe = new File(csvSave);
         String line;
-        String cvsSplitBy = ",";
+        String[] fisher;
+        String value;
         if(0>=a1 || 0>=na)throw new IllegalArgumentException("valeurs négatives");
         if(a1>1000)a1=1000;
         if(na>2000)na=2000;
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+        for (String[] strings : data) {
+            fisher = strings;
+            value = fisher[2].substring(0, fisher[2].length() - 1);
+            if (na == Integer.parseInt(fisher[0]) && a1 == Integer.parseInt(fisher[1])) {
+                return Double.parseDouble(value);
+            }
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(csvSave))) {
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                fisher = line.split(cvsSplitBy);
+                value = fisher[2].substring(0, fisher[2].length() - 1);
+                if (na == Integer.parseInt(fisher[0]) && a1 == Integer.parseInt(fisher[1])) {
+                    data.add(0,fisher);
+                    return Double.parseDouble(value);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile));
+             FileWriter output = new FileWriter(csvSafe)) {
 
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
-                String[] fisher = line.split(cvsSplitBy);
-                String value = fisher[2].substring(0, fisher[2].length()-1);
-                if(na==Integer.parseInt(fisher[0])&& a1==Integer.parseInt(fisher[1]))
+                fisher = line.split(cvsSplitBy);
+                value = fisher[2].substring(0, fisher[2].length()-1);
+                if(na==Integer.parseInt(fisher[0])&& a1==Integer.parseInt(fisher[1])) {
+                    data.add(0,fisher);
+                    for (String[] datum : data) {
+                        fisher = datum;
+                        output.write((fisher[0] + "," + fisher[1] + "," + fisher[2])+"\n");
+                    }
                     return Double.parseDouble(value);
 
+                }
             }
 
         } catch (IOException e) {
